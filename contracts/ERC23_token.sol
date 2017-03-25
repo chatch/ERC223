dddba1b388db3ab4e1c903d834f0b796812ff221
Jorge Izquierdo <jorge@izqui.me>
2017-03-25 12:55:08 +0100

    Start clean up. Add truffle.

--- contracts/ERC23_token.sol
+++ contracts/ERC23_token.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.9;
- 
+
  /* New ERC23 contract interface */
 
 contract ERC23 {
@@ -15,13 +15,12 @@ contract ERC23 {
   event Approval(address indexed owner, address indexed spender, uint value);
 }
 
-
  /*
- * ERC23 token by Dexaran
+ * ERC23 token
  *
  * https://github.com/Dexaran/ERC23-tokens
  */
- 
+
 contract ERC23Token is ERC23 {
 
   mapping(address => uint) balances;
@@ -29,29 +28,15 @@ contract ERC23Token is ERC23 {
 
   //function that is called when a user or another contract wants to transfer funds
   function transfer(address _to, uint _value, bytes _data) returns (bool success) {
-  
     //filtering if the target is a contract with bytecode inside it
-    if(isContract(_to)) {
-        transferToContract(_to, _value, _data);
-    }
-    else {
-        transferToAddress(_to, _value, _data);
-    }
-    return true;
+    if (isContract(_to)) return transferToContract(_to, _value, _data);
+    return transferToAddress(_to, _value, _data);
   }
-  
+
   function transfer(address _to, uint _value) returns (bool success) {
-      
     //standard function transfer similar to ERC20 transfer with no _data
     //added due to backwards compatibility reasons
-    bytes emptyData;
-    if(isContract(_to)) {
-        transferToContract(_to, _value, emptyData);
-    }
-    else {
-        transferToAddress(_to, _value, emptyData);
-    }
-    return true;
+    return transfer(_to, _value, 0x0);
   }
 
   //function that is called when transaction target is an address
@@ -61,7 +46,7 @@ contract ERC23Token is ERC23 {
     Transfer(msg.sender, _to, _value);
     return true;
   }
-  
+
   //function that is called when transaction target is a contract
   function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
     balances[msg.sender] -= _value;
@@ -71,7 +56,7 @@ contract ERC23Token is ERC23 {
     Transfer(msg.sender, _to, _value);
     return true;
   }
-  
+
   //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
   function isContract(address _addr) private returns (bool is_contract) {
       uint length;
@@ -89,7 +74,7 @@ contract ERC23Token is ERC23 {
 
   function transferFrom(address _from, address _to, uint _value) returns (bool success) {
     var _allowance = allowed[_from][msg.sender];
-    
+
     if(_value > _allowance) {
         throw;
     }
