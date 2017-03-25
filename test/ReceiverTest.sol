67e6ea58a106c0f5bde46590120d7d337edae7ae
Jorge Izquierdo <jorge@izqui.me>
2017-03-25 16:06:04 +0100

    Fix

--- test/ReceiverTest.sol
+++ test/ReceiverTest.sol
@@ -15,6 +15,7 @@ contract ReceiverTest {
 
   function testFallbackIsCalledOnTransfer() {
     token.transfer(receiver, 10);
+
     Assert.equal(receiver.tokenAddr(), token, 'Token address should be correct');
     Assert.equal(receiver.tokenSender(), this, 'Sender should be correct');
     Assert.equal(receiver.sentValue(), 10, 'Value should be correct');
@@ -23,6 +24,7 @@ contract ReceiverTest {
   function testCorrectFunctionIsCalledOnTransfer() {
     bytes memory data = new bytes(4); // foo() is 0xc2985578
     data[0] = 0xc2; data[1] = 0x98; data[2] = 0x55; data[3] = 0x78;
+
     token.transfer(receiver, 20, data);
 
     Assert.equal(receiver.tokenSig(), bytes4(0xc2985578), 'Sig should be correct');
